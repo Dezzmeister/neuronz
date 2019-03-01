34de4bf8048cfa27743b5d236e176d29f984fbba
Dezz Meister <dezzy3359@gmail.com>
2019-03-01 18:27:50 -0500

    added a bitwise neural network test

--- Neuronz/src/main/test/network/NetworkTest.java
+++ Neuronz/src/main/test/network/NetworkTest.java
@@ -21,5 +21,33 @@ public class NetworkTest {
 			System.out.println(network.feedForwardAndLearn(input, expected, learningRate));
 		}
 	}
+	
+	/**
+	 * Trains a neural network to perform a bitwise operation
+	 */
+	public static void bitwiseNetworkLearningTest() {
+		Network network = new Network(2, 4, 2);
+		float learningRate = 0.1f;
+		float epochs = 10000;
+		for (int j = 0; j < 10; j++) {
+			int correct = 0;
+			for (int i = 0; i < epochs; i++) {
+				int rand0 = (int)(Math.random() * 2);
+				int rand1 = (int)(Math.random() * 2);
+				int expectedResult = rand0 | rand1;
+				VectorN expected = new VectorN(expectedResult == 1 ? 1 : 0, expectedResult == 0 ? 1 : 0);
+				VectorN input = new VectorN(rand0, rand1);
+				
+				VectorN output = network.feedForwardAndLearn(input, expected, learningRate);
+				
+				int realResult = (output.get(0) > output.get(1)) ? 1 : 0;
+				
+				if (realResult == expectedResult) {
+					correct += 1;
+				}
+			}
+			System.out.println("Percent correct in last 10000 epochs: " + ((100.0f * correct)/10000) + "%");
+		}
+	}
 }
  
